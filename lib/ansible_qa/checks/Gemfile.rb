806bb5bce739b46cccad068e5e32cc59e0bac7cd
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-07 19:34:06 +0900

    explain what to do

--- lib/ansible_qa/checks/Gemfile.rb
+++ lib/ansible_qa/checks/Gemfile.rb
@@ -10,7 +10,10 @@ module AnsibleQA
 
       def check
         must_exist
-        should_be_identical
+        if !should_be_identical
+          info "%s should be identical unless you need additional gems" % [ @path ]
+          info "Update %s with the latest Gemfile created by ansible-role-init"
+        end
       end
 
     end
