1962da8b47f48ae1b29123bd2ba9204dc4dd3c8b
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-14 13:26:28 +0900

    fix simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
-require "simplecov"
-require "coveralls"
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-  add_filter "spec"
-end
-
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "rspec/core/rake_task"
 require "pathname"
