065942b43289572e5d58fbb54be70b109b6a25de
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-15 12:54:42 +0900

    make simplecov and coveralls working again (#12)
    * make simplecov and coveralls working again
    
    * style

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
 require "simplecov"
 require "coveralls"
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+)
 SimpleCov.start do
-  add_filter "spec"
+  add_filter "/spec/"
 end
-Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "rspec/core/rake_task"
