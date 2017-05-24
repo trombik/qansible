1a0f11e6d7fec1ebeccf986a1467eb71f7bd7054
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-05-24 13:37:26 +0900

    add a check

---
+++ lib/qansible/checks/extra_roles.rb
@@ -0,0 +1,21 @@
+module Qansible
+  class check
+    class ExtraRoles < Qansible::Check::Base
+
+      def initialize
+        super(:path => "extra_roles")
+      end
+
+      def check
+        should_not_exist
+      end
+
+      def should_not_exist
+        debug "%s Checking `%s`, which must not exist" % [ self.class.name, @path ]
+        result = File.exist?(@@root + @path)
+        warn "File or directory `%s` should not exist. Run `rm -rf extra_roles` and remove it from `.kitchen.yml`. See https://github.com/trombik/qansible/issues/51 for why" % [ @path ] if !result
+        not result
+      end
+    end
+  end
+end
