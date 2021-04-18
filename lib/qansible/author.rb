91150f52a9a5631d04a4c29700b27b7a953e1a16
Tomoyuki Sakurai <y@trombik.org>
2021-04-18 09:10:35 +0700

    bugfix: do not use Unix password database for full name

--- lib/qansible/author.rb
+++ lib/qansible/author.rb
@@ -4,18 +4,16 @@ require "etc"
 
 module Qansible
   class Author
-    attr_reader :login, :fullname
-    def initialize
-      @login = Etc.getpwuid(Process.uid).name
-      @fullname = Etc.getpwuid(Process.uid).gecos
-    end
+    def initialize; end
 
     def email
       email = `git config --get user.email`
       email.chomp
     end
 
-    alias login_name login
-    alias whoami login
+    def fullname
+      fullname = `git config --get user.name`
+      fullname.chomp
+    end
   end
 end
